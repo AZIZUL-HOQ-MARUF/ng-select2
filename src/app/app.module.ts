fb742bcfe45b7a1e9feac7099c3c3279bfaf1f6c
AZIZUL HOQ <42655898+AZIZUL-HOQ-MARUF@users.noreply.github.com>
2019-03-10 23:55:35 +0600

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicComponent } from './basic/basic.component';
+import { Select2Module } from 'ng2-select2';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      BasicComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      Select2Module
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
